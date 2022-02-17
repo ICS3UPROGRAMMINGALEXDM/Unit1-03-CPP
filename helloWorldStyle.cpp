eba4ca386ffad638ab96f05401d55a5289900c32
Ubuntu <ubuntu@ip-172-31-86-29.ec2.internal>
2022-02-17 22:07:53 +0000

    Finished helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Alex De Meo All rights reserved
+//
+// Created By: Alex De Meo
+// Date: 02/17/22
+// Displays hello world wih style
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Enjoy your day!" << endl;
+}
